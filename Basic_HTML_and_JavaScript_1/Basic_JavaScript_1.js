f5ba0fa1a3c5300f5a9f752e15981c19693779dd
saifsyed0626 <saifsyed0626@gmail.com>
2025-12-10 19:41:10 -0500

    HTML Assignments
    HTML Assignments

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Write title here.</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+<script>
+alert("Hello, World!");
+</script>
+	<body>
+	<button onclick="My_First_Function()">
+		Click me!
+	</button>
+	<p id="Irish"></p>
+<script>
+	function My_First_Function() (
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish") .innerHTML =
+		)
+</script>
+	</body>
+</html>
